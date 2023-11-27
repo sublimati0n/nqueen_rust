58a6a1999b39fc661c75c17e7156b5630cd0ab06
sublimation <hootphilo2d1@gmail.com>
2023-11-28 00:24:43 +0900

    feat-WIP: 実装途中

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate nqueen;
 
+use core::panic;
+use std::{io::IsTerminal, option};
+
 use rand::Rng;
 
 fn show_board(sol: &Vec<usize>) {
@@ -107,69 +110,111 @@ fn construct(sol: &Vec<usize>) -> (Vec<usize>, Vec<usize>) {
         }
         if forelse {
             let mut min_colls = 100000000;
-            let mut col_id = -1;
-            let mut col = -1;
+            let mut col_id: Option<usize> = None;
+            let mut col: Option<usize> = None;
             for j in 0..cand.len() {
                 let n_colls = diag_up[i + cand[j]] + diag_dn[(n - 1) - i + cand[j]];
                 if n_colls < min_colls {
                     min_colls = n_colls;
-                    col = cand[j];
-                    col_id = j
+                    col = Some(cand[j]);
+                    col_id = Some(j);
                 }
             }
-            sol[i] = col;
-            diag_up[i + col] += 1;
-            diag_dn[(n - 1) - i + col] += 1;
-            cand.remove(col_id);
+            match col {
+                None => {
+                    panic!("couldn't update value");
+                }
+                Some(j) => {
+                    sol[i] = j;
+                    diag_up[i + j] += 1;
+                    diag_dn[(n - 1) - i + j] += 1;
+                }
+            }
+            match col_id {
+                None => {
+                    panic!("couldn't update value");
+                }
+                Some(j) => {
+                    cand.remove(j);
+                }
+            }
         }
     }
     (diag_up, diag_dn)
 }
 
-fn fast_tabu_search(sol: &Vec<usize>, diag_up: &Vec<usize>, diag_dn: &Vec<usize>){
+fn fast_tabu_search(sol: &mut Vec<usize>, diag_up: &mut Vec<usize>, diag_dn: &mut Vec<usize>) {
     let n = sol.len();
-    let mut tabu: Vec<usize> = vec![-1, n];
+    let mut tabu: Vec<Option<usize>> = vec![None, Some(n)];
     let max_iter = 100000;
     let mut tabulen = std::cmp::min(10, n);
-    for n_iter in 0..max_iter{
-        for i in range(n - 1, -1, -1):
-            colls = diag_up[i + sol[i]] + diag_dn[(n - 1) - i + sol[i]]
-            if colls - 2 > 0:
-                istar = i
-                break
-        else:  # no collusion, we finish the search
-            break
+    for n_iter in 0..max_iter {
+        let mut forelse = true;
+        let mut i_star: usize = 0;
+        let mut colls_star: usize = 0;
+        for i in (0..n).rev() {
+            let n_colls = diag_up[i + sol[i]] + diag_dn[(n - 1) - i + sol[i]];
+            if n_colls - 2 > 0 {
+                i_star = i;
+                colls_star = n_colls;
+                forelse = false;
+                break;
+            }
+        }
+        if forelse {
+            // no collusion, we finish the search
+            break;
+        }
 
-        # print "swap candidate is",istar
-        delta = -999999
-        jstar = -1
-        for j in range(n):
-            if tabu[j] >= n_iter or j == istar:
-                continue
-            temp = (diag_up[j + sol[j]] + diag_dn[(n - 1) - j + sol[j]] + colls) - (
-                diag_up[istar + sol[j]]
-                + diag_dn[(n - 1) - istar + sol[j]]
-                + diag_up[j + sol[istar]]
-                + diag_dn[(n - 1) - j + sol[istar]]
-            )
-            if temp > delta:
-                delta = temp
-                jstar = j
+        let mut delta = 0;
+        let mut j_star: Option<usize> = None;
+        for j in 0..n {
+            match tabu[j] {
+                None => {
+                    continue;
+                }
+                Some(t) => {
+                    if t >= n_iter || j == i_star {
+                        continue;
+                    }
+                }
+            }
 
-        print("iter=", n_iter, "swap", istar, jstar, "delta=", delta)
-        if jstar == -1:  # clear tabu list
-            tabulen = int(tabulen / 2) + 1
-            tabu = [-1] * n
-        else:
-            tabu[istar] = tabu[jstar] = n_iter + random.randint(1, tabulen)
-            exchange(istar, jstar, sol, diag_up, diag_dn)
+            let temp = (diag_up[j + sol[j]] + diag_dn[(n - 1) - j + sol[j]] + colls_star)
+                - (diag_up[i_star + sol[j]]
+                    + diag_dn[(n - 1) - i_star + sol[j]]
+                    + diag_up[j + sol[i_star]]
+                    + diag_dn[(n - 1) - j + sol[i_star]]);
+            if temp > delta {
+                delta = temp;
+                j_star = Some(j);
+            }
+        }
 
-        if LOG:
-            display(sol)
-            up, dn = diagonals(sol)
-            print("queens on upward diagonals:", up)
-            print("queens on downward diagonals:", dn)
-            ncolls = collisions(up) + collisions(dn)
+        match j_star {
+            None => {
+                // clear tabu list
+                tabulen = tabulen / 2 + 1;
+                let mut tabu: Vec<Option<usize>> = vec![None, Some(n)];
+            }
+            Some(j_star) => {
+                println!(
+                    "iter={}: swap {}&{}, delta={}",
+                    n_iter, i_star, j_star, delta
+                );
+                let val = n_iter + rand::thread_rng().gen_range(1..tabulen);
+                tabu[i_star] = Some(val);
+                tabu[j_star] = Some(val);
+                exchange(i_star, j_star, sol, diag_up, diag_dn);
+            }
+        }
+
+        // if LOG:
+        //     display(sol)
+        //     up, dn = diagonals(sol)
+        //     print("queens on upward diagonals:", up)
+        //     print("queens on downward diagonals:", dn)
+        //     ncolls = collisions(up) + collisions(dn)
     }
 
     ()
