ff955f02a47335ce4ba9055e7d9a6db5ffa28898
Tetsushi Saburi <tetsushi.saburi.j7a@jp.denso.com>
2023-11-28 18:53:10 +0900

    feat-WIP: 実行できるようにはなったが何かおかしい

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate nqueen;
 
 use core::panic;
-use std::{io::IsTerminal, option};
+use std::{env, io::IsTerminal, option, process::exit};
 
 use rand::Rng;
 
@@ -18,6 +18,16 @@ fn show_board(sol: &Vec<usize>) {
         }
         println!();
     }
+}
+
+fn show_log(sol: &Vec<usize>, diag_up: &Vec<usize>, diag_dn: &Vec<usize>) {
+    println!("array: {:?}", sol);
+    show_board(sol);
+    println!("queens on upward diagonals: {:?}", diag_up);
+    println!("queens on downward diagonals: {:?}", diag_dn);
+    let n_colls = collisions(diag_up) + collisions(diag_dn);
+    println!("#collisions: {}", n_colls);
+    println!();
 }
 
 fn diagonals(sol: &Vec<usize>) -> (Vec<usize>, Vec<usize>) {
@@ -81,7 +91,7 @@ fn exchange(
     diag_dn[d] += 1;
 }
 
-fn construct(sol: &Vec<usize>) -> (Vec<usize>, Vec<usize>) {
+fn construct(sol: &mut Vec<usize>) -> (Vec<usize>, Vec<usize>) {
     let n = sol.len();
     let n_diag = 2 * n - 1;
 
@@ -145,7 +155,7 @@ fn construct(sol: &Vec<usize>) -> (Vec<usize>, Vec<usize>) {
 
 fn fast_tabu_search(sol: &mut Vec<usize>, diag_up: &mut Vec<usize>, diag_dn: &mut Vec<usize>) {
     let n = sol.len();
-    let mut tabu: Vec<Option<usize>> = vec![None, Some(n)];
+    let mut tabu: Vec<Option<usize>> = vec![None; n];
     let max_iter = 100000;
     let mut tabulen = std::cmp::min(10, n);
     for n_iter in 0..max_iter {
@@ -195,7 +205,7 @@ fn fast_tabu_search(sol: &mut Vec<usize>, diag_up: &mut Vec<usize>, diag_dn: &mu
             None => {
                 // clear tabu list
                 tabulen = tabulen / 2 + 1;
-                let mut tabu: Vec<Option<usize>> = vec![None, Some(n)];
+                tabu = vec![None; n];
             }
             Some(j_star) => {
                 println!(
@@ -208,26 +218,33 @@ fn fast_tabu_search(sol: &mut Vec<usize>, diag_up: &mut Vec<usize>, diag_dn: &mu
                 exchange(i_star, j_star, sol, diag_up, diag_dn);
             }
         }
-
-        // if LOG:
-        //     display(sol)
-        //     up, dn = diagonals(sol)
-        //     print("queens on upward diagonals:", up)
-        //     print("queens on downward diagonals:", dn)
-        //     ncolls = collisions(up) + collisions(dn)
     }
-
-    ()
 }
 
 fn main() {
-    let LOG = false;
+    let log: bool = true;
 
-    let n = 10;
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("$1: #queens");
+        exit(1);
+    }
 
-    let sol: Vec<_> = (0..n).collect();
+    let n: usize = args[1].parse().unwrap();
 
-    println!("{:?}", sol);
+    let mut sol: Vec<_> = (0..n).collect();
 
-    show_board(&sol);
+    let (mut up, mut dn) = construct(&mut sol);
+
+    println!("--------- Initial solution (random greedy) ---------");
+    if log {
+        show_log(&sol, &up, &dn);
+    }
+
+    println!("--------- starting fast tabu search ---------");
+    fast_tabu_search(&mut sol, &mut up, &mut dn);
+    println!("--------- finish fast tabu search ---------");
+    if log {
+        show_log(&sol, &up, &dn);
+    }
 }
